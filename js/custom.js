8b6546434c9820660aa86f2364f5a842db29fb3b
Zac Maguire <zactmaguire@gmail.com>
2015-04-10 15:51:45 +1200

    updated layout, added story and cast sections to index.html, setting logos in place too 10 April

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+
+// Author: Zachary Maguire
+// Custom JS for Lords of dogtown index
+// Project 3 
+
+
+
+
+$(document).ready(function(){
+
+
+	// Show and hide Story Content
+
+		$( ".thirdbox" ).click(function() {
+		  $( ".story-content" ).toggle( "slow", function() {
+		    // Animation complete.
+		  });
+		});
+
+	// Show and hide Cast Content
+
+		$( ".forthbox" ).click(function() {
+		  $( ".cast-content" ).toggle( "slow", function() {
+		    // Animation complete.
+		  });
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
